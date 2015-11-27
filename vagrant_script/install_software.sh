7e11e244bfe1c56f1bc916f1951505ae2d9afdea
Alexey Lebedeff <alebedev@mirantis.com>
2015-11-27 12:29:14 +0300

    Fix apt-proxy usage

--- vagrant_script/install_software.sh
+++ vagrant_script/install_software.sh
@@ -3,8 +3,8 @@ set -ex
 exec 2>&1 
 
 APT_PROXY_URL=$1
-if [[ -z $APT_PROXY_URL ]]; then
-    echo Acquire::http::Proxy \"$APT_PROXY_URL\" > /etc/apt/apt.conf.d/proxy
+if [[ ! -z $APT_PROXY_URL ]]; then
+    echo Acquire::http::Proxy \"$APT_PROXY_URL\"\; > /etc/apt/apt.conf.d/proxy
 fi
 
 apt-get update
